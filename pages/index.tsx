3043f47a6b44fe9946da54c046ef3528518ace39
TomMahe <mahettom@gmail.com>
2023-07-17 14:46:34 +0200

    Avatar + Name | Done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
 import Image from "next/image"
-
+import data from '../data.json'
 
 
 export default function Home() {
 
 
   return (
-    <h1 className="text-3xl font-bold underline">Hello World</h1>
+    <div className='flex flex-col items-center mx-auto w-full mt-16'>
+      <Image
+        className='rounded-full'
+        src={data.avatar}
+        width={96}
+        height={96}
+        alt={data.name}
+      />
+      <h1 className='font-bold mt-4 text-xl'>{data.name}</h1>
+    </div>
   )
 }
