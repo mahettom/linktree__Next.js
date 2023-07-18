9f583db87a515a1444ea71d7643f43d5a2539971
TomMahe <mahettom@gmail.com>
2023-07-18 17:31:51 +0200

    delete pages folder to pass to app structure

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css'
-import { AppProps } from "next/app"
-
-// ——————————————————————————————————————————————————————————————— KIND OF THE GLOBAL LAYOUT OF THE APP
-// ——————————————————————————————————————————————————————————————— (WHERE ALL THE MAGIC OF NEXT HAPPEN)
-
-export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
-}
