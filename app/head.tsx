e3087713ef7f4d339ce4057fc0d6149e1b469fe4
TomMahe <mahettom@gmail.com>
2023-07-20 12:19:54 +0200

    delete <link> favicon

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,6 @@ export default function Head() {
         <>
             <title>Tom's Linktree</title>
             <meta name="description" content="Next.js + Tailwind CSS" />
-            <link rel="icon" href="/favicon.ico" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
         </>
     );
